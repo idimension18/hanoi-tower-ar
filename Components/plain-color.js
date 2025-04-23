61032c32648d85109d873723db2bc7b91962c7ec
idimension <41388029+idimension18@users.noreply.github.com>
2025-04-23 12:13:57 +0200

    added first components yay

---
+++ Components/plain-color.js
@@ -0,0 +1,21 @@
+AFRAME.registerComponent('plain-color', {
+  schema: {
+    color: {type: 'color', default: '#FFFFFF'}
+  },
+
+  init: function () {
+    this.el.addEventListener("model-loaded", e => { // when using gltf models use "model-loaded" instead
+      const material = new THREE.MeshPhongMaterial({
+        color: this.data.color
+      });
+
+      const object = this.el.getObject3D('mesh');
+      if (object) {
+        object.traverse(function (node) {
+          if (node.isMesh) node.material = material;
+        });
+      };
+    });
+  },
+
+});
